3ed7cc276c3fc41a2b8243a051fd2542e70ac975
ismaelnascimento <ismael.nascimento08@aluno.ifce.edu.br>
2025-02-17 14:52:34 -0300

    Atualizações na tela de criação de atividade - component datepicker

---
+++ ViewComponents/TextAreaComponent.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+import SwiftData
+
+struct InputComponent: View {
+    var label: String
+    var placeholder: String
+    @Binding var value: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .font(.system(size: 14, weight: .medium))
+                .foregroundColor(.colorText)
+                .padding(.bottom, -6)
+            HStack {
+                TextField(placeholder, text: $value)
+                    .frame(height: 40)
+                    .padding(EdgeInsets(top: 0, leading: Sizes.padding, bottom: 0, trailing: Sizes.padding))
+                    
+                    .foregroundColor(Color.colorText)
+                      .font(.system(size: 14))
+                      .multilineTextAlignment(.leading)
+            }.background(Color.colorForeground)
+            .cornerRadius(Sizes.radius)
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var inputValue: String = ""
+    
+    InputComponent(label: "Nome", placeholder: "Digite o nome", value: $inputValue)
+}
